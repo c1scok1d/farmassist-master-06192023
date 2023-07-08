f21925b37097b7a79886d068cc02da7eeeefb250
c1scok1d <rchatman@macinternetservices.com>
2023-07-08 18:40:36 -0500

    added static category

--- lib/data/farm/resources/repository.dart
+++ lib/data/farm/resources/repository.dart
@@ -4,6 +4,6 @@ import 'newsAPI.dart';
 
 class Repository {
   final moviesApiProvider = NewsApiProvider();
-  Future<List<Article>> fetchAllNews({String category = ''}) =>
+  Future<List<Article>> fetchAllNews({String category = 'Science'}) =>
       moviesApiProvider.fetchNewsList(category: category);
 }
